395b98641bf6bc970afc0c779dd953716d1aaead
pipkin0265 <71162077+pipkin0265@users.noreply.github.com>
2021-09-06 23:27:35 -0400

    Start Over

--- MonstersSuchAsUs/Program.cs
+++ MonstersSuchAsUs/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MonstersSuchAsUs
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            MakeTitle();
+        }
+
+        public static void MakeTitle()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("##     ##  #######  ##    ##  ######  ######## ######## ########   ######      ######  ##     ##  ######  ##     ##       ###     ######     ##     ##  ######  ");
+            Console.WriteLine("###   ### ##     ## ###   ## ##    ##    ##    ##       ##     ## ##    ##    ##    ## ##     ## ##    ## ##     ##      ## ##   ##    ##    ##     ## ##    ## ");
+            Console.WriteLine("#### #### ##     ## ####  ## ##          ##    ##       ##     ## ##          ##       ##     ## ##       ##     ##     ##   ##  ##          ##     ## ##       ");
+            Console.WriteLine("## ### ## ##     ## ## ## ##  ######     ##    ######   ########   ######      ######  ##     ## ##       #########    ##     ##  ######     ##     ##  ######  ");
+            Console.WriteLine("##     ## ##     ## ##  ####       ##    ##    ##       ##   ##         ##          ## ##     ## ##       ##     ##    #########       ##    ##     ##       ## ");
+            Console.WriteLine("##     ## ##     ## ##   ### ##    ##    ##    ##       ##    ##  ##    ##    ##    ## ##     ## ##    ## ##     ##    ##     ## ##    ##    ##     ## ##    ## ");
+            Console.WriteLine("##     ##  #######  ##    ##  ######     ##    ######## ##     ##  ######      ######   #######   ######  ##     ##    ##     ##  ######      #######   ######  \n\n");
+
+            Console.WriteLine("                                                                     Press Start To Enter                                                                                      ");
+            Console.ReadKey();
         }
     }
 }
