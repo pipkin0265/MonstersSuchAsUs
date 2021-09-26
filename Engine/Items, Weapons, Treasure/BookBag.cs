a89713e47e49540ed887b30573926deeb924c5b5
Rileyd1888 <52174567+Rileyd1888@users.noreply.github.com>
2021-09-26 07:51:46 -0400

    Added Items, Treasures and Potions

---
+++ Engine/Items, Weapons, Treasure/BookBag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Items__Weapons__Treasure
+{
+    class BookBag : Item
+    {
+        public int Bag_number { get; set; }
+        public string Bag_Name { get; set; }
+
+        public string BookBag_Description { get; set; }
+
+    }
+}
---
+++ Engine/Items, Weapons, Treasure/MonsterCrystal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Items__Weapons__Treasure
+{
+    class MonsterCrystal : Item
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
