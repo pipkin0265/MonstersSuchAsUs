e96946b7fe27f95ae056e1644289c63b87ed8815
Michael Pipkin II <Michael Pipkin II@LAPTOP-0FB8OVSQ>
2021-09-26 20:08:17 -0400

    another test

---
+++ Engine/Combat/RollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class RollDice
+    {
+        private int _diceNum;
+        private int _sizeDice;
+        private int _diceEnd;
+        Random random = new Random();
+
+        public int DiceNum { get { return _diceNum; } set { _diceNum = value; } }
+        public int DiceSize { get { return _sizeDice; } set { _sizeDice = value; } }
+        public int DiceEnd { get { Roll(); return _diceEnd; } }
+
+        public RollDice(string typeDice)
+        {
+            string[] dice = typeDice.Split('D');
+            int num = Convert.ToInt32(dice[0]);
+            int die = Convert.ToInt32(dice[1]);
+
+            DiceNum = num;
+            DiceSize = die;
+        }
+
+        private void Roll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
