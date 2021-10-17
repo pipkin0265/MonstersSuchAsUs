d7b616b2c9b8dcea56402384d8c5f663a9796f8d
Rileyd1888 <52174567+Rileyd1888@users.noreply.github.com>
2021-10-17 10:08:58 -0400

    Commiting info for Databases

---
+++ Engine/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace Engine
+{
+   public class DataAccess
+    {
+        public List<Item> GetItems(int id, string name, string description)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("Engine.Properties.Settings.DerekMonstersSuchAsUsConnectionString")))
+            {
+                
+                var output = connection.Query<Item>("dbo.Items").ToList();
+                return output;
+            }
+        }
+    }
+}
